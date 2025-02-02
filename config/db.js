7bf88c9c81884f3d2cae9da11ecb1f4f958b4499
unknown <akashprinceakash9986@gmail.com>
2025-02-02 17:17:26 +0530

    Initial Commit 3

--- config/db.js
+++ config/db.js
@@ -15,9 +15,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     ssl: {
-        rejectUnauthorized: false // Required for Render PostgreSQL
+        rejectUnauthorized: false // Required for cloud PostgreSQL
     }
 });
 
