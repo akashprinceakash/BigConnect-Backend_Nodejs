45dcc464f851427cd821153f5abb213035705c97
unknown <akashprinceakash9986@gmail.com>
2025-02-02 17:23:00 +0530

    Initial Commit 4

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const userRoutes = require("./routes/userRoutes");
 require("dotenv").config();
 const admin = require("./config/firebase");  // Import Firebase Admin
 const app = express();
-
+const pool = require('./config/db'); // Import your PostgreSQL connection
 app.use(cors());
 app.use(express.json());
 
@@ -19,6 +19,15 @@ app.get('/generate-token', async (req, res) => {
   });
   
 
+  app.get('/test-db', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()'); // Get current time from DB
+        res.json({ message: "Connected to PostgreSQL", time: result.rows[0] });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Database connection failed" });
+    }
+});
 
 // Use user routes
 app.use("/api", userRoutes);
